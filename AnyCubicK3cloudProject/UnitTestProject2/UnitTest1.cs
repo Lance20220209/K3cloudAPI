1b9a3653d5e04874033f64bdc6c397f24bda057e
Lance20220209 <lancehuang@anycubic.com>
2022-05-31 11:38:24 +0800

    修改20220531

--- AnyCubicK3cloudProject/UnitTestProject2/UnitTest1.cs
+++ AnyCubicK3cloudProject/UnitTestProject2/UnitTest1.cs
@@ -10,7 +10,7 @@ namespace UnitTestProject2
         [TestMethod]
         public void TestMethod1()
         {
-            //我在vs进行修wo 的订单22
+            //我在vs进行修金蝶修改
             ZWLF_CreateErpOrderTest zWLF_CreateErpOrderTest = new ZWLF_CreateErpOrderTest();
             zWLF_CreateErpOrderTest.CreateErpOrderTest();
 
